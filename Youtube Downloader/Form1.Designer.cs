50b5af43c62837b46ddf03b681128a41d91130ed
zechaos <36123003+zechaos031@users.noreply.github.com>
2020-10-14 20:46:04 +0200

    Add files via upload

---
+++ Youtube Downloader/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Youtube_Downloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(420, 64);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "YouTube - URL:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(400, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 84);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(420, 60);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Converter:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {"mp3", "mp4"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 24);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(400, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.Text = "Format";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 292);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(420, 52);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Telecharger";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.richTextBox1);
+            this.groupBox3.Location = new System.Drawing.Point(12, 152);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(420, 136);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Status:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(8, 20);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(404, 108);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(449, 357);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "YouTube Downloader";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
---
+++ Youtube Downloader/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using VideoLibrary;
+
+namespace Youtube_Downloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length == 0)
+            {
+                MessageBox.Show("Veuillez entré une vidéo.");
+                return;
+            }
+
+            if (comboBox1.Text != "mp3" && comboBox1.Text != "mp4")
+            {
+                MessageBox.Show("Veuillez entré un format de vidéo.");
+                return;
+            }
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog() { Description = "Ton chemin" })
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        richTextBox1.AppendText(Environment.NewLine + "Capture des information de la video...");
+
+                        var youtube = YouTube.Default;
+                        
+                        var video = await youtube.GetVideoAsync(textBox1.Text);
+
+                        if (comboBox1.Text == "mp3")
+                        {
+                            richTextBox1.AppendText(Environment.NewLine + "Téléchargement en cours...");
+                            File.WriteAllBytes(fbd.SelectedPath + @"\" + video.FullName + ".mp3",
+                                await video.GetBytesAsync());
+                            richTextBox1.AppendText(Environment.NewLine + "Téléchargement fini dans: " + fbd.SelectedPath +
+                                                    @"\" + video.FullName);
+                        } else if (comboBox1.Text == "mp4")
+                        {
+                            richTextBox1.AppendText(Environment.NewLine + "Téléchargement en cours...");
+
+                            File.WriteAllBytes(fbd.SelectedPath + @"\" + video.FullName + ".mp4", await video.GetBytesAsync());
+                            richTextBox1.AppendText(Environment.NewLine + "Téléchargement fini dans: " + fbd.SelectedPath + @"\" + video.FullName);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Veuillez entré un format de vidéo.");
+                        }
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Failed to decrypt URL");
+                    }
+                    
+
+
+                    
+                }
+            }
+        }
+    }
+}
